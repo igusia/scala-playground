df4833c1881ea1b0e9e028d84681b4613c3227db
igusia <ir405504@icm.edu.pl>
2019-03-09 17:41:32 +0100

    playground scala

--- src/main/scala/part1basics/CallBy.scala
+++ src/main/scala/part1basics/CallBy.scala
@@ -1,5 +1,19 @@
 package part1basics
 
-object CallBy {
+object CallBy extends App {
+
+  def calledByValue(x: Long): Unit = {
+    println(x)
+    println(x)
+  }
+
+  def calledByName(x: => Long): Unit = {
+    println(x)
+    println(x)
+  }
+
+  calledByValue(System.nanoTime())
+  calledByName(System.nanoTime())
+
 
 }
